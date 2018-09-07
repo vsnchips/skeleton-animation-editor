3909e6641226b2feab9e1d08be7be0bf2fb93ca3
Daniel Aston <tonupkiwi@gmail.com>
2018-09-08 04:58:46 +1200

    Handed in. built. dusted.

---
+++ b2/src/CMakeFiles/a2-loader.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shelly/local/CGRA350/assignment2/srctree/include/skeleton/skeleton.cpp" "/home/shelly/local/CGRA350/assignment2/b2/src/CMakeFiles/a2-loader.dir/__/include/skeleton/skeleton.cpp.o"
+  "/home/shelly/local/CGRA350/assignment2/srctree/src/cgra/imgui_impl_glfw_gl3.cpp" "/home/shelly/local/CGRA350/assignment2/b2/src/CMakeFiles/a2-loader.dir/cgra/imgui_impl_glfw_gl3.cpp.o"
+  "/home/shelly/local/CGRA350/assignment2/srctree/src/cgra/mesh.cpp" "/home/shelly/local/CGRA350/assignment2/b2/src/CMakeFiles/a2-loader.dir/cgra/mesh.cpp.o"
+  "/home/shelly/local/CGRA350/assignment2/srctree/src/cgra/shader.cpp" "/home/shelly/local/CGRA350/assignment2/b2/src/CMakeFiles/a2-loader.dir/cgra/shader.cpp.o"
+  "/home/shelly/local/CGRA350/assignment2/srctree/src/cgra/wavefront.cpp" "/home/shelly/local/CGRA350/assignment2/b2/src/CMakeFiles/a2-loader.dir/cgra/wavefront.cpp.o"
+  "/home/shelly/local/CGRA350/assignment2/srctree/src/ex1.cpp" "/home/shelly/local/CGRA350/assignment2/b2/src/CMakeFiles/a2-loader.dir/ex1.cpp.o"
+  "/home/shelly/local/CGRA350/assignment2/srctree/src/main.cpp" "/home/shelly/local/CGRA350/assignment2/b2/src/CMakeFiles/a2-loader.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CGRA_SRCDIR=\"/home/shelly/local/CGRA350/assignment2/srctree\""
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/shelly/local/CGRA350/assignment2/srctree/vendor/glfw/include"
+  "/home/shelly/local/CGRA350/assignment2/srctree"
+  "/home/shelly/local/CGRA350/assignment2/srctree/include"
+  "/usr/include/gtk-3.0"
+  "/usr/include/pango-1.0"
+  "/usr/include/glib-2.0"
+  "/usr/lib/glib-2.0/include"
+  "/usr/include/fribidi"
+  "/usr/include/cairo"
+  "/usr/include/pixman-1"
+  "/usr/include/freetype2"
+  "/usr/include/libpng16"
+  "/usr/include/harfbuzz"
+  "/usr/include/uuid"
+  "/usr/include/gdk-pixbuf-2.0"
+  "/usr/include/gio-unix-2.0"
+  "/usr/include/libdrm"
+  "/usr/include/atk-1.0"
+  "/usr/include/at-spi2-atk/2.0"
+  "/usr/include/at-spi-2.0"
+  "/usr/include/dbus-1.0"
+  "/usr/lib/dbus-1.0/include"
+  "/home/shelly/local/CGRA350/assignment2/srctree/src"
+  "/home/shelly/local/CGRA350/assignment2/srctree/include/skeleton"
+  "/home/shelly/local/CGRA350/assignment2/srctree/vendor/glew/include"
+  "/home/shelly/local/CGRA350/assignment2/srctree/vendor/glm"
+  "/home/shelly/local/CGRA350/assignment2/srctree/vendor/imgui"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/shelly/local/CGRA350/assignment2/b2/vendor/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/shelly/local/CGRA350/assignment2/b2/vendor/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  "/home/shelly/local/CGRA350/assignment2/b2/vendor/imgui/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
